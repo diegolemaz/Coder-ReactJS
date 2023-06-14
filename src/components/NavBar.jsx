e8e7111e93359991d5ff40f1a1c8d4dd127a6e2c
Diego <dlz85@outlook.com>
2023-06-13 22:12:06 -0300

    Inicio App

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,18 @@
+import carrIm from "../assets/carrito.png"
+export const NavBar = ( ) => {
+
+    <header>
+        <nav>
+            <img src={carrIm} alt="Carrito" /> 
+           <h6>M@RKET</h6> 
+          <ul>
+            <li>
+                <a href= "*">INICIO</a>
+                <a href= "*">PRODUCTOS</a>
+                <a href= "*">CARRITO</a>
+                <a href= "*">ACERCA DE NOSOTROS</a>
+            </li>
+            </ul>  
+        </nav>
+    </header>
+   }
